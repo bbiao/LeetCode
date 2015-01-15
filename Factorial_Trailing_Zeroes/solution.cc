6f0d50bfdbc57eb6f215b7c7105316e8abbfaf60
ZHANG Biao <bbbiao@gmail.com>
2015-01-15 20:05:30 +0800

    Factorial Trailing Zeroes && Excel Sheet Column Number

---
+++ Factorial_Trailing_Zeroes/solution.cc
@@ -0,0 +1,14 @@
+/**
+ * Given an integer n, return the number of trailing zeroes in n!.
+ * Note: Your solution should be in logarithmic time complexity.
+ */
+
+class Solution {
+public:
+    int trailingZeroes(int n) {
+        if (n < 5) {
+            return 0;
+        }
+        return n / 5 + trailingZeroes(n / 5);
+    }
+};
